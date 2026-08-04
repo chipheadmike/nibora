af07e7786e70fad996b13a052cf55c28c56f1421
Mike Williams <diesel.kelly@gmail.com>
2026-08-04 07:11:17 -0400

    Initial Commit

---
+++ Nibora/NiboraApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiboraApp.swift
+//  Nibora
+//
+//  Created by Mike Williams on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct NiboraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NiboraTests/NiboraTests.swift
@@ -0,0 +1,19 @@
+//
+//  NiboraTests.swift
+//  NiboraTests
+//
+//  Created by Mike Williams on 8/4/26.
+//
+
+import Testing
+@testable import Nibora
+
+struct NiboraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
